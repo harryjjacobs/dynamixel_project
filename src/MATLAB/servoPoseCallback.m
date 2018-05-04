9a0646dee40b4a4b1f1c9546edb6884a572aa957
Harry <harryjjacobs@googlemail.com>
2018-05-04 13:55:08 +0100

    fix: matlab set servo position

--- src/MATLAB/servoPoseCallback.m
+++ src/MATLAB/servoPoseCallback.m
@@ -1,19 +1,8 @@
 function servoPoseCallback(~, message)
-    %exampleHelperROSPoseCallback Subscriber callback function for pose data    
-    %   exampleHelperROSPoseCallback(~,MESSAGE) returns no arguments - it instead sets 
-    %   global variables to the values of position and orientation that are
-    %   received in the ROS message MESSAGE.
-    %   
-    %   See also ROSPublishAndSubscribeExample.
-    
-    %   Copyright 2014-2015 The MathWorks, Inc.
-    
-    % Declare global variables to store position and orientation
+    % Declare global variables to store
     global pos
-    global orient
     
     % Extract position and orientation from the ROS message and assign the
     % data to the global variables.
     pos = [message.Linear.X message.Linear.Y message.Linear.Z];
-    orient = [message.Angular.X message.Angular.Y message.Angular.Z];
 end
--- src/MATLAB/servo_control.m
+++ src/MATLAB/servo_control.m
@@ -4,10 +4,15 @@ rostopic list
 %motors = rossubscriber('/joint1_controller/pan_tilt_port')
 %motorstatelist = receive(motors,10)
 
-joint1 = rospublisher
+joint4 = rospublisher('joint4_controller/command', 'std_msgs/Float64')
+msg = rosmessage(joint4);
+msg.Data = 0;
+send(joint4, msg)
+
 
 %robotpose = rossubscriber('/pose',@servoPoseCallback)
 
 % run these in command window to stop
 %clear robotpose
+pause(3);
 rosshutdown
