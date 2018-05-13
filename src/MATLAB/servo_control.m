50674577a6a53595ccc7955cb2a25a77d17c194a
harryjjacobs <harryjjacobs@googlemail.com>
2018-05-14 00:03:00 +0100

    update: working forward kinematics

--- src/MATLAB/servo_control.m
+++ src/MATLAB/servo_control.m
@@ -14,6 +14,7 @@ halfpi = pi/2;
 minangles = [minangle; minangle/4; minangle/4; minangle/4; minangle];
 maxangles = [maxangle; maxangle/4; maxangle/4; maxangle/2; maxangle];
 
+
 publishers = [
     rospublisher('joint1_controller/command', 'std_msgs/Float64');
     rospublisher('joint2_controller/command', 'std_msgs/Float64');
@@ -22,6 +23,7 @@ publishers = [
     rospublisher('joint5_controller/command', 'std_msgs/Float64');
 ];
 
+%{
 subscribers = [
     rossubscriber('joint1_controller/state');
     rossubscriber('joint2_controller/state');
@@ -37,25 +39,23 @@ pause(5) % Wait to ensure publisher is registered
 msg = rosmessage(emPub);
 msg.Data = true;
 send(emPub, msg);
+%}
 
 l1 = 0.1;
 l2 = 0.1;
-    
-% Define the D-H table for the arm
+
+% Define the D-H table for the robot
 alpha0 = 0; a0=0; d1=0; theta1=0;					% DH table row: i=1
 L(1) = Link([theta1 d1 a0 alpha0 0 0], 'modified'); % We use the modified DH
 
 alpha1 = -90*pi/180; a1=0; d2=0; theta2=0;			% DH table row: i=2
 L(2) = Link([theta2 d2 a1 alpha1 0 0], 'modified'); % We use the modified DH
 
-alpha2 = 0; a2=l1; d3=0; theta3=0;			% DH table row: i=2
-L(3) = Link([theta3 d3 a2 alpha2 0 0], 'modified'); % We use the modified DH
+alpha3 = 0; a3=l1; d4=0; theta4=0;			% DH table row: i=4
+L(3) = Link([theta4 d4 a3 alpha3 0 0], 'modified'); % We use the modified DH
 
-alpha3 = 0; a3=l2; d4=0; theta4=0;			% DH table row: i=2
-L(4) = Link([theta4 d4 a3 alpha3 0 0], 'modified'); % We use the modified DH
-
-alpha4 = -90*pi/180; a4=1; d5=0; theta5=0;			% DH table row: i=2
-L(5) = Link([theta5 d5 a4 alpha4 0 0], 'modified'); % We use the modified DH
+alpha4 = -90*pi/180; a4=l2; d5=0; theta5=0;			% DH table row: i=5
+L(4) = Link([theta5 d5 a4 alpha4 0 0], 'modified'); % We use the modified DH
 
 arm = SerialLink(L, 'name', 'Arm')
 
@@ -79,8 +79,14 @@ setJointAngle(angles(1), publishers(3));
 setJointAngle(angles(3), publishers(1));
 %}
 
-pause(3);
-angles = getJointAngles(subscribers(1))
+pause(2);
+%angles = getJointAngles(subscribers)
+angles = [0 0 0 0]
+t = arm.fkine([angles(1) angles(2) angles(3) angles(4)]).t
+position(1) = t(1);
+position(2) = t(2);
+position(3) = t(3);
+position
 
 %robotpose = rossubscriber('/pose',@servoPoseCallback)
 % run these in command window to stop
