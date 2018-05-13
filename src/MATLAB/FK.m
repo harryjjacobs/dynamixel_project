4c78f96b2d7175bdd39d12b6a90825dde5b353db
harryjjacobs <harryjjacobs@googlemail.com>
2018-05-14 00:03:33 +0100

    update: forward kinematics for 5 link arm

--- src/MATLAB/FK.m
+++ src/MATLAB/FK.m
@@ -1,14 +1,24 @@
-function pos = FK(Theta1, Theta2, Theta3)
-    alpha0 = 0; a0=0; d1=3; theta1=0;					% DH table row: i=1
+function pos = FK(Theta1, Theta2, Theta3, Theta4)
+    l1 = 0.1;
+    l2 = 0.1;
+
+    % Define the D-H table for the arm
+    alpha0 = 0; a0=0; d1=0; theta1=0;					% DH table row: i=1
     L(1) = Link([theta1 d1 a0 alpha0 0 0], 'modified'); % We use the modified DH
 
-    alpha1 = -90*pi/180; a1=1; d2=2; theta2=0;			% DH table row: i=2
-    L(2) = Link([theta2 d2 a1 alpha1 0 -90*pi/180], 'modified'); % We use the modified DH
+    alpha1 = -90*pi/180; a1=0; d2=0; theta2=0;			% DH table row: i=2
+    L(2) = Link([theta2 d2 a1 alpha1 0 0], 'modified'); % We use the modified DH
 
-    alpha2 = 90*pi/180; a2=0; d3=0; theta3=0;			% DH table row: i=3
-    L(3) = Link([theta3 d3 a2 alpha2 1 2], 'modified'); % We use the modified DH
+    alpha2 = 0; a2=l1; d3=0; theta3=0;                  % DH table row: i=3
+    L(3) = Link([theta3 d3 a2 alpha2 0 0], 'modified'); % We use the modified DH
 
-    RRP_robot = SerialLink(L, 'name', 'RRP Robot')
+    alpha3 = 0; a3=l2; d4=0; theta4=0;                  % DH table row: i=4
+    L(4) = Link([theta4 d4 a3 alpha3 0 0], 'modified'); % We use the modified DH
+
+    alpha4 = -90*pi/180; a4=1; d5=0; theta5=0;			% DH table row: i=5
+    L(5) = Link([theta5 d5 a4 alpha4 0 0], 'modified'); % We use the modified DH
     
+    arm = SerialLink(L, 'name', 'Arm');
     
+    pos = arm.fkine([Theta1, Theta2, Theta3, Theta4])
 end
